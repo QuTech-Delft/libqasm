5219fd5b3f51eae2a0f46d9890c82c476f635e46
Jeroen van Straten <jeroen.van.straten@gmail.com>
2021-06-17 20:01:03 +0200

    Fix annotation data for variables

--- src/cqasm/src/cqasm-v1-analyzer.cpp
+++ src/cqasm/src/cqasm-v1-analyzer.cpp
@@ -1443,6 +1443,7 @@ void AnalyzerHelper::analyze_variables(const ast::Variables &variables) {
             // record where the variable was defined.
             auto var = tree::make<semantic::Variable>(identifier->name, type.clone());
             var->copy_annotation<parser::SourceLocation>(*identifier);
+            var->annotations = analyze_annotations(variables.annotations);
             result.root->variables.add(var);
 
             // Add a mapping for the variable.
