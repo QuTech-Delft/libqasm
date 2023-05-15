e8f66ea10797a76f9955e34e404b3a6b944677da
rturrado <rturrado@gmail.com>
2023-05-15 13:03:18 +0200

    Moved cqasm-version.cpp from test/v3 to test.

--- test/v3/parsing/grammar/version.cpp
+++ test/v3/parsing/grammar/version.cpp
@@ -2,13 +2,4 @@
 #include <gtest/gtest.h>
 
 
-TEST(version, v_1_0) {}
-TEST(version, v_1_1) {}
-TEST(version, v_1_1_1) {}
-TEST(version, v_1_2) {}
-TEST(version, v_1_3) {}
-TEST(version, v_2_0) {}
-TEST(version, v_3_0) {}
-TEST(version, no_version) {}
-TEST(version, v_abc) {}
-TEST(version, v_1_1_abc) {}
+// Add tests here
