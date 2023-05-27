c85b00ac174ba26ea2769c6d9a25c5cd499ad877
rturrado <rturrado@gmail.com>
2023-05-27 18:12:30 +0200

    Updated conanfile.py with fixes needed by the libqasm recipe. Updated CMake minimum required version to 3.20 (for cxx_std_23).

--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,15 @@
-from conan import ConanFile, tools
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
 from conan.tools.scm import Git
 
 class LibqasmConan(ConanFile):
     name = "libqasm"
-    version = "0.5.0"
 
     # Optional metadata
     license = "Apache-2.0"
-    author = "QuTech-Delft"
-    url = "https://center.conan.io"
+    homepage = "https://github.com/QuTech-Delft/libqasm"
+    url = "https://github.com/conan-io/conan-center-index"
     description = "Library to parse cQASM files"
     topics = ("code generation", "parser", "compiler", "quantum compilation", "quantum simulation")
 
@@ -45,12 +45,12 @@ class LibqasmConan(ConanFile):
             del self.options.fPIC
 
     def layout(self):
-        cmake_layout(self)
+        cmake_layout(self, src_folder="src")
 
     def source(self):
         git = Git(self)
         git.clone(url="https://github.com/QuTech-Delft/libqasm.git", target=".")
-        git.checkout("4aba125ee41ac8b95435957456694236ad218f7c")
+        git.checkout("2db9916f8e32c5e36d05cb20cafb87133d4dbf16")
 
     def generate(self):
         deps = CMakeDeps(self)
@@ -66,6 +66,10 @@ class LibqasmConan(ConanFile):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, "23")
 
     def package(self):
         cmake = CMake(self)
