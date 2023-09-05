2211662f7de233087bd8159309897e047e26f936
rturrado <rturrado@gmail.com>
2023-09-05 10:27:16 +0200

    Addressed some of the comments from Pablo's code review. - antlr-4.13.0-complete.jar: removed. - generate_antlr_parser.py: updated to download ANTLR jar file to the system's temporary directory.

--- scripts/generate_antlr_parser.py
+++ scripts/generate_antlr_parser.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+from urllib.request import urlopen
+from urllib import error
+
 import os
 import subprocess
 import sys
+import tempfile
 
 
 def print_usage():
@@ -14,31 +18,62 @@ def print_usage():
     print("    generate_antlr_parser.py ../src/v3x ../build/Debug/src/v3x")
 
 
-def generate_antlr_parser(input_folder, output_folder):
+class AntlrJarFileDownloader:
+    file_version = "4.13.1"
+    file_name = "antlr4-{}-complete.jar".format(file_version)
+    file_path = "{}/{}".format(tempfile.gettempdir(), file_name)
+
+    @classmethod
+    def exists_antlr_jar_file(cls):
+        return os.path.exists(cls.file_path)
+
+    @classmethod
+    def download_antlr_jar_file(cls):
+        url = "https://repo1.maven.org/maven2/org/antlr/antlr4/{}/{}".format(cls.file_version, cls.file_name)
+        try:
+            print("Downloading ANTLR jar file from '{}'".format(url))
+            with urlopen(url) as response:
+                print("Writing ANTLR jar file to '{}'".format(cls.file_path))
+                with open(cls.file_path, "wb") as f:
+                    f.write(response.read())
+        except error.URLError as err:
+            print("Error downloading ANTLR jar file: {}".format(err.strerror))
+            exit(3)
+
+    @classmethod
+    def get_antlr_jar_file_path(cls):
+        if not cls.exists_antlr_jar_file():
+            cls.download_antlr_jar_file()
+        return cls.file_path
+
+
+def generate_antlr_parser(input_folder, output_folder, antlr_jar_file_path):
     print("Generating ANTLR lexer and parser files")
-    current_folder_path = os.path.dirname(os.path.abspath(__file__))
-    jar_file_path = os.path.join(current_folder_path, "..", "3rd_party", "antlr-4.13.0-complete.jar")
-    java = subprocess.run([
-        "java",
-        "-jar",
-        jar_file_path,
-        "-Xexact-output-dir",
-        "-o",
-        "{}".format(output_folder),
-        "-Dlanguage=Cpp",
-        "{}/cqasm_lexer.g4".format(input_folder),
-        "{}/cqasm_parser.g4".format(input_folder)
-    ])
-    if java.returncode != 0:
-        print("Error running java: {}", java.returncode)
-        exit(3)
+    try:
+        completed_process = subprocess.run([
+            "java",
+            "-jar",
+            antlr_jar_file_path,
+            "-Xexact-output-dir",
+            "-o",
+            "{}".format(output_folder),
+            "-Dlanguage=Cpp",
+            "{}/cqasm_lexer.g4".format(input_folder),
+            "{}/cqasm_parser.g4".format(input_folder)
+        ])
+        if completed_process.returncode != 0:
+            print("Error generating ANTLR lexer and parser files: {}", completed_process.returncode)
+            exit(5)
+    except FileNotFoundError as err:
+        print("Error running java: {}".format(err.strerror))
+        exit(4)
 
 
 def main(argv):
     if len(argv) != 3:
         print_usage()
         exit(2)
-    generate_antlr_parser(argv[1], argv[2])
+    generate_antlr_parser(argv[1], argv[2], AntlrJarFileDownloader.get_antlr_jar_file_path())
 
 
 if __name__ == '__main__':
