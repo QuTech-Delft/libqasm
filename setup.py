484304553f8d276a024a8de4adb28efeab248164
pablolh <p.lehenaff@tudelft.nl>
2023-09-14 14:19:27 +0200

    Add Python3.10 and 3.11, remove Python < 3.8

--- setup.py
+++ setup.py
@@ -180,10 +180,10 @@ setup(
         'Operating System :: Microsoft :: Windows',
 
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
 
         'Topic :: Scientific/Engineering'
     ],
