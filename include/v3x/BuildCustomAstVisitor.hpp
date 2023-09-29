a1a31dbc4eaba3639d79457699296d1566721537
rturrado <68099809+rturrado@users.noreply.github.com>
2023-09-29 23:19:14 +0200

    BuildCustomAstVisitor class can be empty.

--- include/v3x/BuildCustomAstVisitor.hpp
+++ include/v3x/BuildCustomAstVisitor.hpp
@@ -8,19 +8,6 @@
 
 namespace cqasm::v3x::parser {
 
-class BuildCustomAstVisitor : public CqasmParserVisitor {
-public:
-    virtual std::any visitProgram(CqasmParser::ProgramContext *context) = 0;
-    virtual std::any visitVersion(CqasmParser::VersionContext *context) = 0;
-    virtual std::any visitQubits(CqasmParser::QubitsContext *context) = 0;
-    virtual std::any visitMapping(CqasmParser::MappingContext *context) = 0;
-    virtual std::any visitVariable(CqasmParser::VariableContext *context) = 0;
-    virtual std::any visitInstruction(CqasmParser::InstructionContext *context) = 0;
-    virtual std::any visitExpressionList(CqasmParser::ExpressionListContext *context) = 0;
-    virtual std::any visitIntegerLiteral(CqasmParser::IntegerLiteralContext *context) = 0;
-    virtual std::any visitFloatLiteral(CqasmParser::FloatLiteralContext *context) = 0;
-    virtual std::any visitIdentifier(CqasmParser::IdentifierContext *context) = 0;
-    virtual std::any visitIndex(CqasmParser::IndexContext *context) = 0;
-};
+class BuildCustomAstVisitor : public CqasmParserVisitor {};
 
 }  // namespace cqasm::v3x::parser
