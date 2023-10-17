4ddd9c9a8c70c92811117120e758b5b42dc971a8
rturrado <68099809+rturrado@users.noreply.github.com>
2023-10-17 12:20:13 +0200

    Removed unused import statement 'get'.

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
-from conan.tools.files import copy, get
+from conan.tools.files import copy
 from conan.tools.scm import Version
 
 
