49bc8ecc49e0cbec8350bff2a5ce1028bb67bf40
rturrado <68099809+rturrado@users.noreply.github.com>
2023-11-10 14:18:51 +0100

    Added python/module/cqasm/v3x folder.
    python/CMakeLists.txt:
    - Changed CMake minimum required version to 3.12.
    - Changed C++ standard to C++20 (like for libqasm).
    - Removed code that checked if CMake version was smaller than 3.12.
    - Deleted python/compat folder, which was only used when CMake version was smaller than 3.12.
    - Added v3x Python generated files to installation.
    
    Moved include/cqasm-py.hpp to include/v1x.
    Moved src/cqasm-py.cpp to src/v1x, and refactored code.
    Added include/v3x/cqasm-py.hpp.
    Added src/v3x/cqasm-py.cpp.

---
+++ python/module/cqasm/v3x/instruction.py
@@ -0,0 +1,52 @@
+import cqasm.v3x.types
+
+
+class Instruction(object):
+    def __init__(
+        self,
+        name,
+        types=None,
+        request_qubit_and_bit_indices_have_same_size=False
+    ):
+        super().__init__()
+        self._name = str(name)
+        if types is None:
+            self._types = ()
+        else:
+            for typ in types:
+                if not isinstance(typ, cqasm.v3x.types.Node):
+                    raise TypeError('types must be an iterable of cqasm.v3x.types.Node if specified')
+            self._types = tuple(types)
+        self._request_qubit_and_bit_indices_have_same_size = bool(request_qubit_and_bit_indices_have_same_size)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def types(self):
+        return self._types
+
+    @property
+    def request_qubit_and_bit_indices_have_same_size(self):
+        return self._request_qubit_and_bit_indices_have_same_size
+
+    def __str__(self):
+        return '{}({})'.format(self._name, ', '.join(map(str, self._types)))
+
+
+class InstructionRef(object):
+    def __init__(self, *args, **kwargs):
+        if not args and not kwargs:
+            self._data = None
+        elif len(args) == 1 and not kwargs and (isinstance(args[0], Instruction) or args[0] is None):
+            self._data = args[0]
+        else:
+            self._data = Instruction(*args, **kwargs)
+
+    @property
+    def data(self):
+        return self._data
+
+    def __str__(self):
+        return 'unresolved' if self._data is None else str(self._data)
---
+++ src/v3x/cqasm-py.cpp
@@ -0,0 +1,116 @@
+/** \file
+ * Implementation for the internal Python-wrapped functions and classes.
+ */
+
+#include "cqasm-version.hpp"
+#include "v3x/cqasm-py.hpp"
+#include "v3x/cqasm.hpp"
+#include "v3x/cqasm-parse-helper.hpp"
+
+#include <memory>
+
+namespace v3x = cqasm::v3x;
+
+/**
+ * Creates a new v3.x semantic analyzer.
+ * When without_defaults is specified,
+ * the default instruction set and error models are not loaded into the instruction and error model tables,
+ * so you have to specify the entire instruction set using register_instruction() and register_error_model().
+ * Otherwise, those functions only add to the defaults.
+ * Unlike the C++ version of the analyzer class,
+ * the initial mappings and functions are not configurable at all.
+ * The defaults for these are always used.
+ */
+V3xAnalyzer::V3xAnalyzer(const std::string &max_version, bool without_defaults) {
+    if (without_defaults) {
+        a = std::make_unique<v3x::analyzer::Analyzer>(max_version);
+        a->register_default_mappings();
+    } else {
+        a = std::make_unique<v3x::analyzer::Analyzer>(v3x::default_analyzer(max_version));
+    }
+}
+
+/**
+ * Registers an instruction type.
+ * The arguments are passed straight to instruction::Instruction's constructor.
+ */
+void V3xAnalyzer::register_instruction(
+    const std::string &name,
+    const std::string &param_types,
+    bool request_qubit_and_bit_indices_have_same_size
+) {
+    a->register_instruction(
+        name,
+        param_types,
+        request_qubit_and_bit_indices_have_same_size
+    );
+}
+
+/**
+ * Only parses the given file.
+ * The file must be in v3.x syntax.
+ * No version check or conversion is performed.
+ * Returns a vector of strings,
+ * of which the first is always present and is the CBOR serialization of the v3.x AST.
+ * Any additional strings represent error messages.
+ */
+std::vector<std::string> V3xAnalyzer::parse_file(
+    const std::string &filename
+) {
+    auto result = v3x::parser::parse_file(filename);
+    return result.errors.empty()
+        ? std::vector<std::string>{ tree::base::serialize(result.root) }
+        : result.errors;
+}
+
+/**
+ * Same as parse_file(), but instead receives the file contents directly.
+ * The filename, if specified, is only used when reporting errors.
+ */
+std::vector<std::string> V3xAnalyzer::parse_string(
+    const std::string &data,
+    const std::string &filename
+) {
+    auto result = v3x::parser::parse_string(data, filename);
+    return result.errors.empty()
+        ? std::vector<std::string>{ tree::base::serialize(result.root) }
+        : result.errors;
+}
+
+/**
+ * Parses and analyzes the given file.
+ * If the file is written in a later file version,
+ * this function may try to reduce it to the maximum v3.x API version support advertised
+ * using this object's constructor.
+ * Returns a vector of strings,
+ * of which the first is always present and is the CBOR serialization of the v3x semantic tree.
+ * Any additional strings represent error messages.
+ */
+std::vector<std::string> V3xAnalyzer::analyze_file(
+    const std::string &filename
+) const {
+    auto result = a->analyze(
+        [=](){ return cqasm::version::parse_file(filename); },
+        [=](){ return v3x::parser::parse_file(filename); }
+    );
+    return result.errors.empty()
+        ? std::vector<std::string>{ tree::base::serialize(result.root) }
+        : result.errors;
+}
+
+/**
+ * Same as analyze_file(), but instead receives the file contents directly.
+ * The filename, if specified, is only used when reporting errors.
+ */
+std::vector<std::string> V3xAnalyzer::analyze_string(
+    const std::string &data,
+    const std::string &filename
+) const {
+    auto result = a->analyze(
+        [=](){ return cqasm::version::parse_string(data, filename); },
+        [=](){ return v3x::parser::parse_string(data, filename); }
+    );
+    return result.errors.empty()
+        ? std::vector<std::string>{ tree::base::serialize(result.root) }
+        : result.errors;
+}
