044f1a75a60241688515129fce0d0a716d04954f
rturrado <68099809+rturrado@users.noreply.github.com>
2023-11-16 21:52:50 +0100

    Updated v3x parsing tests to check the JSON dump. I have only enabled the checks for one test for the time being. Since these are file-based checks, enabling them for all the tests may be quite time consuming.

--- test/v3x/parsing.cpp
+++ test/v3x/parsing.cpp
@@ -32,20 +32,32 @@ public:
         cq3x::parser::ParseResult parse_result{};
         parse_result = cq3x::parser::parse_string(input, "input.cq");
 
-        // Check the parse result
-        std::string ast_actual_file_contents{};
-        if (parse_result.errors.empty()) {
-            ast_actual_file_contents = fmt::format("SUCCESS\n{}\n", *parse_result.root);
-        } else {
-            ast_actual_file_contents = fmt::format("ERROR\n{}\n", fmt::join(parse_result.errors, "\n"));
-        }
-
-        auto ast_actual_file_path = path_ / "ast.actual.txt";
-        cqasm::test::write_file(ast_actual_file_path, ast_actual_file_contents);
+        // Check the debug dump of the parse result
+        std::string ast_actual_file_contents = parse_result.errors.empty()
+            ? fmt::format("SUCCESS\n{}\n", *parse_result.root)
+            : fmt::format("ERROR\n{}\n", fmt::join(parse_result.errors, "\n"));
         std::string ast_golden_file_contents{};
+        auto ast_actual_file_path = path_ / "ast.actual.txt";
         auto ast_golden_file_path = path_ / "ast.golden.txt";
+        cqasm::test::write_file(ast_actual_file_path, ast_actual_file_contents);
         EXPECT_TRUE(cqasm::test::read_file(ast_golden_file_path, ast_golden_file_contents));
         EXPECT_TRUE(ast_actual_file_contents == ast_golden_file_contents);
+
+        // Check the JSON dump of the parse result
+        if (parse_result.errors.empty()) {
+            if (auto json_golden_file_path = path_ / "ast.golden.json"; fs::exists(json_golden_file_path)) {
+                auto json_actual_file_path = path_ / "ast.actual.json";
+                std::string json_actual_file_contents{};
+                std::string json_golden_file_contents{};
+                {
+                    std::ofstream json_actual_ofs{ json_actual_file_path };
+                    parse_result.root->dump_json(json_actual_ofs);
+                }
+                EXPECT_TRUE(cqasm::test::read_file(json_actual_file_path, json_actual_file_contents));
+                EXPECT_TRUE(cqasm::test::read_file(json_golden_file_path, json_golden_file_contents));
+                EXPECT_TRUE(json_actual_file_contents == json_golden_file_contents);
+            }
+        }
 
         // Stop if parsing failed
         if (!parse_result.errors.empty()) {
@@ -86,19 +98,33 @@ public:
             // Run the actual semantic analysis
             auto analysis_result = analyzer.analyze(*parse_result.root->as_program());
 
-            // Check the analysis results
-            std::string semantic_actual_file_contents{};
-            if (analysis_result.errors.empty()) {
-                semantic_actual_file_contents = fmt::format("SUCCESS\n{}\n", *analysis_result.root);
-            } else {
-                semantic_actual_file_contents = fmt::format("ERROR\n{}\n", fmt::join(analysis_result.errors, "\n"));
-            }
+            // Check the debug dump of the analysis result
+            std::string semantic_actual_file_contents = analysis_result.errors.empty()
+                ? fmt::format("SUCCESS\n{}\n", *analysis_result.root)
+                : fmt::format("ERROR\n{}\n", fmt::join(analysis_result.errors, "\n"));
             auto semantic_actual_file_path = path_ / fmt::format("semantic.{}.actual.txt", api_version);
+            auto semantic_golden_file_path = path_ / fmt::format("semantic.{}.golden.txt", api_version);
             cqasm::test::write_file(semantic_actual_file_path, semantic_actual_file_contents);
             std::string semantic_golden_file_contents{};
-            auto semantic_golden_file_path = path_ / fmt::format("semantic.{}.golden.txt", api_version);
             EXPECT_TRUE(cqasm::test::read_file(semantic_golden_file_path, semantic_golden_file_contents));
             EXPECT_TRUE(semantic_actual_file_contents == semantic_golden_file_contents);
+
+            // Check the JSON dump of the analysis result
+            if (analysis_result.errors.empty()) {
+                if (auto json_golden_file_path = path_ / fmt::format("semantic.{}.golden.json", api_version);
+                    fs::exists(json_golden_file_path)) {
+                    auto json_actual_file_path = path_ / fmt::format("semantic.{}.actual.json", api_version);
+                    std::string json_actual_file_contents{};
+                    std::string json_golden_file_contents{};
+                    {
+                        std::ofstream json_actual_ofs{ json_actual_file_path };
+                        analysis_result.root->dump_json(json_actual_ofs);
+                    }
+                    EXPECT_TRUE(cqasm::test::read_file(json_actual_file_path, json_actual_file_contents));
+                    EXPECT_TRUE(cqasm::test::read_file(json_golden_file_path, json_golden_file_contents));
+                    EXPECT_TRUE(json_actual_file_contents == json_golden_file_contents);
+                }
+            }
 
             if (analysis_result.errors.empty()) {
                 ::tree::base::serialize(analysis_result.root);
