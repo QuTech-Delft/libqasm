644132b7974d4a61adb6144a098d3854d2ef7e14
rturrado <68099809+rturrado@users.noreply.github.com>
2023-11-28 15:31:19 +0100

    test_parse_string is now recognized when running pytest. We still have the same problem though as we have for the json_parser branch: AttributeError: module 'libQasm' has no attribute 'V3xAnalyzer'.

--- test/test_init.py
+++ test/test_init.py
@@ -1,28 +1,28 @@
 import os
 import unittest
 
+import libQasm
+
 
 class TestLibQasm(unittest.TestCase):
     @staticmethod
     def _generic_test_steps(file_name):
-        import libQasm as qasm_interface
         base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         data_file = os.path.join(base_dir, 'res', 'v10', file_name + '.qasm')
-        lib_qasm = qasm_interface.libQasm()
+        lib_qasm = libQasm.libQasm()
         lib_qasm.parse_file(data_file)
         return lib_qasm
 
     @staticmethod
     def _generic_test_steps_as_string(file_name):
-        import libQasm as qasm_interface
         base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         data_file = os.path.join(base_dir, 'res', 'v10', file_name + '.qasm')
         with open(data_file, 'r') as qasm_file:
             qasm_string = qasm_file.read()
-        lib_qasm = qasm_interface.libQasm()
+        lib_qasm = libQasm.libQasm()
         lib_qasm.parse_string(qasm_string)
         return lib_qasm
 
     @staticmethod
     def _get_v3x_analyzer():
-        return libQasm.V3xAnalyzer
+        return libQasm.V3xAnalyzer()
--- test/v3x/python/test_v3x_analyzer.py
+++ test/v3x/python/test_v3x_analyzer.py
@@ -1,9 +1,8 @@
 from ...test_init import TestLibQasm
 
 
-# TODO: have this test recognized when running pytest, and implement it
 class TestV3xAnalyzer(TestLibQasm):
-    def parse_string(self):
+    def test_parse_string(self):
         program_str = "version 3;qubit[5] q;bit[5] b;h q[0:4];b = measure q"
         v3x_analyzer = self._get_v3x_analyzer()
         ret = v3x_analyzer.parse_string(program_str)
