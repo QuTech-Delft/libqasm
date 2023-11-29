4560a40a337917441a1cd673ef4d0dc4ab17ac98
rturrado <68099809+rturrado@users.noreply.github.com>
2023-11-29 12:44:10 +0100

    Fixed issue: `AttributeError: 'string' object has no attribute 'encode'. With code: return ast.Program.deserialize(retval[0].encode("utf-8", errors="surrogateescape")) We are now converting retval[0] to a string before encoding.

--- python/module/cqasm/v3x/__init__.py
+++ python/module/cqasm/v3x/__init__.py
@@ -1,35 +1,46 @@
 import libQasm
 
 
-class V3xAnalyzer(libQasm.V3xAnalyzer):
+class Analyzer(libQasm.V3xAnalyzer):
 
     @staticmethod
     def parse_file(*args):
-        retval = libQasm.V3xAnalyzer.parse_file(*args)
-        if len(retval) == 1:
+        ret = libQasm.V3xAnalyzer.parse_file(*args)
+        if len(ret) == 1:
             import cqasm.v3x.ast as ast
-            return ast.Root.deserialize(retval[0].encode("utf-8", errors="surrogateescape"))
-        return list(retval[1:])
+            serialized_ast_str = str(ret[0])
+            serialized_ast_bytes = serialized_ast_str.encode(encoding='utf-8', errors="surrogateescape")
+            deserialized_ast = ast.Root.deserialize(serialized_ast_bytes)
+            return deserialized_ast
+        return list(ret[1:])
 
     @staticmethod
     def parse_string(*args):
-        retval = libQasm.V3xAnalyzer.parse_string(*args)
-        if len(retval) == 1:
+        ret = libQasm.V3xAnalyzer.parse_string(*args)
+        if len(ret) == 1:
             import cqasm.v3x.ast as ast
-            return ast.Root.deserialize(retval[0].encode("utf-8", errors="surrogateescape"))
-        return list(retval[1:])
+            serialized_ast_str = str(ret[0])
+            serialized_ast_bytes = serialized_ast_str.encode(encoding='utf-8', errors="surrogateescape")
+            deserialized_ast = ast.Root.deserialize(serialized_ast_bytes)
+            return deserialized_ast
+        return list(ret[1:])
 
     def analyze_file(self, *args):
-        retval = super().analyze_file(*args)
-        if len(retval) == 1:
+        ret = super().analyze_file(*args)
+        if len(ret) == 1:
             import cqasm.v3x.semantic as semantic
-            print(retval[0].encode("utf-8", errors="surrogateescape"))
-            return semantic.Program.deserialize(retval[0].encode("utf-8", errors="surrogateescape"))
-        return list(retval[1:])
+            serialized_ast_str = str(ret[0])
+            serialized_ast_bytes = serialized_ast_str.encode(encoding='utf-8', errors="surrogateescape")
+            deserialized_ast = semantic.Root.deserialize(serialized_ast_bytes)
+            return deserialized_ast
+        return list(ret[1:])
 
     def analyze_string(self, *args):
-        retval = super().analyze_string(*args)
-        if len(retval) == 1:
+        ret = super().analyze_string(*args)
+        if len(ret) == 1:
             import cqasm.v3x.semantic as semantic
-            return semantic.Program.deserialize(retval[0].encode("utf-8", errors="surrogateescape"))
-        return list(retval[1:])
+            serialized_ast_str = str(ret[0])
+            serialized_ast_bytes = serialized_ast_str.encode(encoding='utf-8', errors="surrogateescape")
+            deserialized_ast = semantic.Root.deserialize(serialized_ast_bytes)
+            return deserialized_ast
+        return list(ret[1:])
