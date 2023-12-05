17aec50f4dfd28c37e145166a5ffd349b854316b
rturrado <rturrado@gmail.com>
2023-12-05 18:25:13 +0100

    Fixed errors in test/cqasm-utils.cpp. Introduced when merging develop into json_dumper branch.

--- test/cqasm-utils.cpp
+++ test/cqasm-utils.cpp
@@ -30,7 +30,7 @@ TEST(equal_case_insensitive, different_strings) { EXPECT_FALSE(equal_case_insens
 TEST(to_json, v1x_parser_errors) {
     auto input_file_path = fs::path{"res"}/"v1x"/"parsing"/"grammar"/"expression_recovery"/"input.cq";
     cqasm::v1x::analyzer::Analyzer analyzer{};
-    auto semantic_ast_result = analyzer.analyze(input_file_path.generic_string());
+    auto semantic_ast_result = analyzer.analyze_file(input_file_path.generic_string());
     auto json_result = to_json(semantic_ast_result);
     auto expected_json_result = std::string{
         R"delim({"errors":["res/v1x/parsing/grammar/expression_recovery/input.cq:4:21: syntax error, unexpected ','","res/v1x/parsing/grammar/expression_recovery/input.cq:4:31: syntax error, unexpected ','","res/v1x/parsing/grammar/expression_recovery/input.cq:4:37: syntax error, unexpected ')', expecting ']'","res/v1x/parsing/grammar/expression_recovery/input.cq:4:61: syntax error, unexpected ','","res/v1x/parsing/grammar/expression_recovery/input.cq:4:67: syntax error, unexpected ')', expecting ',' or ']'","Failed to parse res/v1x/parsing/grammar/expression_recovery/input.cq"]})delim"
@@ -48,7 +48,7 @@ TEST(to_json, v1x_parser_ast) {
 }
 TEST(to_json, v1x_analyzer_errors) {
     auto input_file_path = fs::path{"res"}/"v1x"/"parsing"/"misc"/"wait_not_ok_1"/"input.cq";
-    auto semantic_ast_result = cqasm::v1x::default_analyzer().analyze(input_file_path.generic_string());
+    auto semantic_ast_result = cqasm::v1x::default_analyzer().analyze_file(input_file_path.generic_string());
     auto json_result = to_json(semantic_ast_result);
     auto expected_json_result = std::string{
         R"delim({"errors":["Error at res/v1x/parsing/misc/wait_not_ok_1/input.cq:4:1..7: failed to resolve overload for wait with argument pack (int)"]})delim"
@@ -57,7 +57,7 @@ TEST(to_json, v1x_analyzer_errors) {
 }
 TEST(to_json, v1x_analyzer_ast) {
     auto input_file_path = fs::path{"res"}/"v1x"/"parsing"/"grammar"/"map"/"input.cq";
-    auto semantic_ast_result = cqasm::v1x::default_analyzer().analyze(input_file_path.generic_string());
+    auto semantic_ast_result = cqasm::v1x::default_analyzer().analyze_file(input_file_path.generic_string());
     auto json_result = to_json(semantic_ast_result);
     auto expected_json_result = std::string{
         R"delim({"Program":{"api_version":"1.0","version":{"Version":{"items":"1.0","source_location":"res/v1x/parsing/grammar/map/input.cq:1:9..12"}},"num_qubits":"10","error_model":"-","subcircuits":"[]","mappings":[{"Mapping":{"name":"three","value":{"ConstInt":{"value":"3","source_location":"res/v1x/parsing/grammar/map/input.cq:4:5..6"}},"annotations":[{"AnnotationData":{"interface":"first","operation":"annot","operands":"[]","source_location":"res/v1x/parsing/grammar/map/input.cq:4:15..26"}}],"source_location":"res/v1x/parsing/grammar/map/input.cq:4:1..26"}},{"Mapping":{"name":"also_three","value":{"ConstInt":{"value":"3","source_location":"res/v1x/parsing/grammar/map/input.cq:5:18..23"}},"annotations":[{"AnnotationData":{"interface":"second","operation":"annot","operands":"[]","source_location":"res/v1x/parsing/grammar/map/input.cq:5:25..37"}},{"AnnotationData":{"interface":"third","operation":"annot","operands":"[]","source_location":"res/v1x/parsing/grammar/map/input.cq:5:39..50"}}],"source_location":"res/v1x/parsing/grammar/map/input.cq:5:1..50"}}],"variables":"[]","source_location":"res/v1x/parsing/grammar/map/input.cq:1:1..6:1"}})delim"
@@ -68,7 +68,7 @@ TEST(to_json, v1x_analyzer_ast) {
 TEST(to_json, v3x_parser_errors) {
     auto input_file_path = fs::path{"res"}/"v3x"/"parsing"/"bit_array_definition"/"bit_array_of_3_14"/"input.cq";
     cqasm::v3x::analyzer::Analyzer analyzer{};
-    auto semantic_ast_result = analyzer.analyze(input_file_path.generic_string());
+    auto semantic_ast_result = analyzer.analyze_file(input_file_path.generic_string());
     auto json_result = to_json(semantic_ast_result);
     auto expected_json_result = std::string{
         R"delim({"errors":["<unknown>:3:5: mismatched input '3.14' expecting INTEGER_LITERAL"]})delim"
@@ -86,7 +86,7 @@ TEST(to_json, v3x_parser_ast) {
 }
 TEST(to_json, v3x_analyzer_errors) {
     auto input_file_path = fs::path{"res"}/"v3x"/"parsing"/"bit_array_definition"/"bit_array_of_0_b"/"input.cq";
-    auto semantic_ast_result = cqasm::v3x::default_analyzer().analyze(input_file_path.generic_string());
+    auto semantic_ast_result = cqasm::v3x::default_analyzer().analyze_file(input_file_path.generic_string());
     auto json_result = to_json(semantic_ast_result);
     auto expected_json_result = std::string{
         R"delim({"errors":["Error at <unknown>:3:8..9: declaring bit array of size <= 0"]})delim"
@@ -95,7 +95,7 @@ TEST(to_json, v3x_analyzer_errors) {
 }
 TEST(to_json, v3x_analyzer_ast) {
     auto input_file_path = fs::path{"res"}/"v3x"/"parsing"/"bit_array_definition"/"bit_array_of_17_b"/"input.cq";
-    auto semantic_ast_result = cqasm::v3x::default_analyzer().analyze(input_file_path.generic_string());
+    auto semantic_ast_result = cqasm::v3x::default_analyzer().analyze_file(input_file_path.generic_string());
     auto json_result = to_json(semantic_ast_result);
     auto expected_json_result = std::string{
         R"delim({"Program":{"api_version":"3.0","version":{"Version":{"items":"3"}},"statements":"[]","variables":[{"Variable":{"name":"b","typ":{"BitArray":{"size":"17"}},"annotations":"[]"}}]}})delim"
