8d3ae6f0d1d4ec46f997b05a923e9d0784d27158
rturrado <68099809+rturrado@users.noreply.github.com>
2023-12-06 13:53:42 +0100

    Fixed get_version binding.

--- emscripten/emscripten_wrapper.hpp
+++ emscripten/emscripten_wrapper.hpp
@@ -37,7 +37,7 @@ struct EmscriptenWrapper {
 EMSCRIPTEN_BINDINGS(CqasmJS) {
     emscripten::class_<EmscriptenWrapper>("EmscriptenWrapper")
     .constructor()
-    .function("parse_string_to_json", &EmscriptenWrapper::get_version)
+    .function("get_version", &EmscriptenWrapper::get_version)
     .function("parse_string_to_json", &EmscriptenWrapper::parse_string_to_json)
     .function("analyze_string_to_json", &EmscriptenWrapper::analyze_string_to_json);
 }
