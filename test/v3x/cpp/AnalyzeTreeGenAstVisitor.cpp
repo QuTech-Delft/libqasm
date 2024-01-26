eb2f5962ec7155cd6260f393ea41fa5ea797270f
rturrado <68099809+rturrado@users.noreply.github.com>
2024-01-27 00:56:35 +0100

    Update AnalyzeTreeGenAstVisitor unit tests.

--- test/v3x/cpp/AnalyzeTreeGenAstVisitor.cpp
+++ test/v3x/cpp/AnalyzeTreeGenAstVisitor.cpp
@@ -1,11 +1,11 @@
 #include "cqasm-error.hpp"
-#include "matcher_values.hpp"
 #include "mock_analyzer.hpp"
 #include "v3x/cqasm-ast-gen.hpp"
+#include "v3x/cqasm-values.hpp"
 #include "v3x/AnalyzeTreeGenAstVisitor.hpp"
 
+#include <fmt/format.h>
 #include <gmock/gmock.h>
-#include <numbers>  // pi
 
 namespace analyzer = cqasm::v3x::analyzer;
 namespace ast = cqasm::v3x::ast;
@@ -15,41 +15,52 @@ namespace values = cqasm::v3x::values;
 
 /*
  * visit_function_call(name, arguments) is a private method, but we can test it through visit_function_call(node)
- * This test mocks the call to analyzer_.call_function, and tests the case when that call returns a non-empty value
  */
 TEST(visit_function_call, analyzer_call_function_returns_a_non_empty_value) {
-    const auto &function_name = std::string{ "sin" };
-    const auto &function_arguments = values::Values{ { cqasm::tree::make<values::ConstReal>(std::numbers::pi) } };
+    const auto &function_name = std::string{ "function_that_returns_a_non_empty_value" };
+
     const auto &function_return_value = values::Value{ cqasm::tree::make<values::ConstReal>(0) };
     auto analyzer = analyzer::MockAnalyzer();
-    EXPECT_CALL(analyzer, call_function(function_name, values::ValuesEq(function_arguments)))
+    EXPECT_CALL(analyzer, call_function(::testing::_, ::testing::_))
         .WillOnce(::testing::Return(function_return_value));
     auto visitor = analyzer::AnalyzeTreeGenAstVisitor(analyzer);
 
-    auto name = cqasm::tree::make<ast::Identifier>("sin");
-    auto arguments = cqasm::tree::make<ast::ExpressionList>(cqasm::tree::Any<ast::Expression>{
-        cqasm::tree::make<ast::FloatLiteral>(std::numbers::pi) });
+    auto name = cqasm::tree::make<ast::Identifier>(function_name);
+    auto arguments = cqasm::tree::make<ast::ExpressionList>(cqasm::tree::Any<ast::Expression>{});
     auto function_call = ast::FunctionCall{ name, arguments };
     auto ret = std::any_cast<values::Value>(visitor.visit_function_call(function_call));
     EXPECT_EQ(ret->as_const_real()->value, function_return_value->as_const_real()->value);
 }
 
-/*
- * visit_function_call(name, arguments) is a private method, but we can test it through visit_function_call(node)
- * This test mocks the call to analyzer_.call_function, and tests the case when that call returns an empty value
- */
 TEST(visit_function_call, analyzer_call_function_returns_an_empty_value) {
-    const auto &function_name = std::string{};
-    const auto &function_arguments = values::Values{};
+    const auto &function_name = std::string{ "function_that_returns_an_empty_value" };
+    const auto &error_message = std::string{ "function implementation returned empty value" };
+
     const auto &function_return_value = values::Value{};
     auto analyzer = analyzer::MockAnalyzer();
-    EXPECT_CALL(analyzer, call_function(function_name, values::ValuesEq(function_arguments)))
+    EXPECT_CALL(analyzer, call_function(::testing::_, ::testing::_))
         .WillOnce(::testing::Return(function_return_value));
     auto visitor = analyzer::AnalyzeTreeGenAstVisitor(analyzer);
 
-    auto name = cqasm::tree::make<ast::Identifier>();
+    auto name = cqasm::tree::make<ast::Identifier>(function_name);
     auto arguments = cqasm::tree::make<ast::ExpressionList>(cqasm::tree::Any<ast::Expression>{});
     auto function_call = ast::FunctionCall{ name, arguments };
     EXPECT_THAT([&]() { visitor.visit_function_call(function_call); },
-        ThrowsMessage<error::AnalysisError>(::testing::HasSubstr("function implementation returned empty value")));
+        ThrowsMessage<error::AnalysisError>(::testing::HasSubstr(error_message.c_str())));
+}
+
+TEST(visit_function_call, analyzer_call_function_throws) {
+    const auto &function_name = std::string{ "function_that_is_not_registered" };
+    const auto &error_message = fmt::format("failed to resolve '{}'", function_name);
+
+    auto analyzer = analyzer::MockAnalyzer();
+    EXPECT_CALL(analyzer, call_function(::testing::_, ::testing::_))
+        .WillRepeatedly(::testing::Throw(std::runtime_error{ error_message.c_str() }));
+    auto visitor = analyzer::AnalyzeTreeGenAstVisitor(analyzer);
+
+    auto name = cqasm::tree::make<ast::Identifier>(function_name);
+    auto arguments = cqasm::tree::make<ast::ExpressionList>(cqasm::tree::Any<ast::Expression>{});
+    auto function_call = ast::FunctionCall{ name, arguments };
+    EXPECT_THAT([&]() { visitor.visit_function_call(function_call); },
+        ThrowsMessage<std::runtime_error>(::testing::HasSubstr(error_message.c_str())));
 }
