ae19751aa8e973d37db5cc1a43c1240c62d3f7f0
rturrado <68099809+rturrado@users.noreply.github.com>
2024-03-28 16:26:21 +0100

    Fix JS job.

--- emscripten/test_cqasm.js
+++ emscripten/test_cqasm.js
@@ -12,7 +12,7 @@ wrapper().then(function(result) {
 
     try {
         var output = cqasm.get_version()
-        var expected_output = "0.5.2"
+        var expected_output = "0.6.0"
         console.log("\nThe version of libqasm compiled with emscripten is:", output);
         if (output !== expected_output) {
             console.log("\tExpected output:", expected_output)
