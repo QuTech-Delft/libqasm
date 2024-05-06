0751d363016f8eecca180202103694986df1145b
rturrado <rturrado@gmail.com>
2024-05-06 13:59:55 +0200

    Trying to fix python-macos-arm64 job.

--- conanfile.py
+++ conanfile.py
@@ -52,7 +52,7 @@ class LibqasmConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("tree-gen/1.0.7")
-        if self.settings.arch == "armv8":
+        if self.settings.arch != "armv8":
             self.tool_requires("zulu-openjdk/21.0.1")
         if self.settings.arch == "wasm":
             self.tool_requires("emsdk/3.1.50")
