1bbd55b2890157eee4061a28c7438d1fcbd7a546
rturrado <rturrado@gmail.com>
2024-10-04 15:52:15 +0200

    Fix get_version functions.

--- versioning.py
+++ versioning.py
@@ -7,7 +7,7 @@ def get_version(verbose=False):
 
     root_dir = os.getcwd()  # root of the repository
     inc_dir = os.path.join(root_dir, "include", "libqasm")  # C++ include directory
-    matcher = re.compile('static const char \*version\{ "(.*)" \}')
+    matcher = re.compile('static const char\* version\{ "(.*)" \}')
     version = None
     with open(os.path.join(inc_dir, "versioning.hpp"), "r") as f:
         for ln in f:
