4000459c50763ddd66a8e209d435b41ff962d128
rturrado <rturrado@gmail.com>
2024-10-09 20:45:47 +0200

    Update tree-gen version to 1.0.8.

--- conanfile.py
+++ conanfile.py
@@ -88,7 +88,7 @@ class LibqasmConan(ConanFile):
         self.cpp.build.libdirs = ["."]
 
     def build_requirements(self):
-        self.tool_requires("tree-gen/1.0.8")
+        self.tool_requires("tree-gen/1.0.9")
         if self.settings.arch != "armv8":
             self.tool_requires("zulu-openjdk/21.0.1")
         if self.settings.arch == "wasm":
@@ -110,7 +110,7 @@ class LibqasmConan(ConanFile):
     def requirements(self):
         self.requires("fmt/11.0.2", transitive_headers=True)
         self.requires("range-v3/0.12.0", transitive_headers=True)
-        self.requires("tree-gen/1.0.8", transitive_headers=True, transitive_libs=True)
+        self.requires("tree-gen/1.0.9", transitive_headers=True, transitive_libs=True)
         if not self.settings.arch == "wasm":
             self.requires("antlr4-cppruntime/4.13.1", transitive_headers=True)
 
